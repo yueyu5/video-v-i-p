08a793eb7cbf97f322c5b5cede78013e15f7cbf8
yueyu5 <zhq_234@163.com>
2017-10-05 21:37:05 +0800

    Update online-vip.js

--- online/online-vip.js
+++ online/online-vip.js
@@ -2,7 +2,6 @@ var inum = 1,
 vipOption = "";
 var vipChannl = new Array(
 "http://v.72du.com/api/?url=",
-"http://www.chepeijian.cn/jiexi/vip.php?url=",
 "http://www.82190555.com/index/iqiyi.php?url=",
 "http://yyygwz.com/index.php?url=",
 "http://api.baiyug.cn/vip/index.php?url=",
