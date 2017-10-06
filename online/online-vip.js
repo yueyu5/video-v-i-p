4437d7bbcaf29c783d3f8f824eae0eff05d7c605
yueyu5 <zhq_234@163.com>
2017-10-06 16:50:50 +0800

    Update online-vip.js

--- online/online-vip.js
+++ online/online-vip.js
@@ -21,7 +21,7 @@ var vipChannl = new Array(
 "http://yun.mt2t.com/yun?url="
 );
 for (x in vipChannl) {
-    vipOption = vipOption + '<option onclick="javascript:window.open(document.getElementById("player").href)"; value="' + vipChannl[x] + '">VIP通道' + inum+++'</option>';
+    vipOption = vipOption + '<option value="' + vipChannl[x] + '">VIP通道' + inum+++'</option>';
 }
 function insVIP(emID, rID, Url) {
     var gemID = document.getElementsByClassName(emID)[0];
@@ -54,7 +54,7 @@ function replacePlayer(e, rID, Url) {
 	var tourl=document.getElementById(Url).value;
     playerID.href = e.value + tourl;
   //  window.open(e.value + tourl, rID, 'width=1051,height=592,location=0,menubar=0,scrollbars=0,status=0,toolbar=0');
-	
+	openUrl(e.value+tourl);
 		/////////////////插入点击选择VIP通道后，select恢复默认状态//////////
 	var sid=document.getElementById("vip-select");
 	document.getElementById("vip-text").value="当前：" + sid.options[sid.selectedIndex].text;
